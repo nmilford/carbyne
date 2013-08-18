934f5b3b1c0bc783486e9be6967d96b2beb86de0
Nathan Milford <nathan@milford.io>
2013-08-17 22:43:43 -0400

    well... got it listening on a port.. so thats a start

--- lib/carbyne.ex
+++ lib/carbyne.ex
@@ -1,2 +1,46 @@
 defmodule Carbyne do
+
+  @carbyne_port 2003
+
+  defrecord Metric, path: nil, metric: 0, time: 0
+
+  def run do
+    IO.puts "Carbyne starting up!"
+    server = listen(@carbyne_port)
+    get_metric(server)
+  end
+
+  def listen(port) do
+    { :ok, server } = Socket.listen "tcp://0.0.0.0:#{port}"
+    IO.puts "Listening for TCP connections on port #{port}"
+    server
+  end
+
+  def get_metric(server) do
+    client = server.accept!
+    metric = client.recv!
+    client.close
+    IO.puts String.split(metric)
+    [ path, value, time ] = String.split(metric)
+    IO.puts """
+    Path:   #{path}
+    Metric: #{value}
+    Time:   #{time}
+    """
+    get_metric(server)
+  end
+
 end
+
+
+# To test
+# in the project dir type 
+#
+# iex -S mix
+#
+# In the iex shell call Carbyne.run
+#
+# iex(1)> Carbyne.run
+#
+# In another shell fire of Graphite style metrics.
+# while :; do  echo "servers.loadavg5 $RANDOM $(date +%s)" | nc 127.0.0.1 2003; sleep 3 ; done
--- mix.exs
+++ mix.exs
@@ -10,12 +10,12 @@ defmodule Carbyne.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [ applications: [:socket] ]
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{ :socket, "0.1.2",  github: "meh/elixir-socket" }]
   end
 end
